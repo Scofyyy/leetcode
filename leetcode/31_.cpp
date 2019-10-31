0eabf54f39ed5f342522a255a7272ad7071318ad
Scofyyy <gongj18@mails.tsinghua.edu.cn>
2019-10-31 14:21:28 +0800

    11
    11

---
+++ leetcode/31_.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	void nextPermutation(vector<int>& nums) {
+		int size = nums.size();
+		int i;
+		for (i = size - 2; i >= 0; --i) {
+			if (nums[i] < nums[i + 1]) {
+				break;
+			}
+		}
+		if (i >= 0) {
+			for (int j = size - 1; j >= i; --j) {
+				if (nums[j] > nums[i]) {
+					swap(nums[j], nums[i]);
+					break;
+				}
+			}
+		}
+		reverse(nums.begin() + i + 1, nums.end());
+	}
+};
