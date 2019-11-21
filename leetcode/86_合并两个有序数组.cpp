57529fa6f7ceeb001874ae4f5af100dc3f0b2161
Scofyyy <gongj18@mails.tsinghua.edu.cn>
2019-11-21 11:21:38 +0800

    Create 86_合并两个有序数组.cpp
    111

---
+++ leetcode/86_合并两个有序数组.cpp
@@ -0,0 +1,16 @@
+/*将两个有序数组合并*/
+//思路：双指针法，从后往前，只在nums1的基础上操作
+#include <vector>
+using namespace std;
+class Solution{
+    void mergeTwoArray(vector<int> nums1,int m,vector<int> nums2,int n){
+
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0&&j>=0)
+        {
+            
+        }
+    }
+}
