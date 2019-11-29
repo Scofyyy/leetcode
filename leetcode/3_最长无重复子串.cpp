d33f8c29faacaca101ba931a4f31085ead68e4b2
scofyyy <gongj18@mails.tsinghua.edu.cn>
2019-11-29 16:32:05 +0800

    111

--- leetcode/3_最长无重复子串.cpp
+++ leetcode/3_最长无重复子串.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 public:
-	int lengthOfLongestSubstring(str s) {
+	int lengthOfLongestSubstring(string s) {
 		int n = 0, left = 0;
 		map<char, int> m;
 		for (int i = 0; i < s.length(); i++) 
