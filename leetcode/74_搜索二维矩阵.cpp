15ca1038190addd47b1a45f600500640c586d5f5
scofyyy <gongj18@mails.tsinghua.edu.cn>
2019-12-05 22:31:01 +0800

    11

--- leetcode/74_搜索二维矩阵.cpp
+++ leetcode/74_搜索二维矩阵.cpp
@@ -1,23 +1,25 @@
-#include<vector>
+#include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty()||matrix[0].empty())
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        if (matrix.empty() || matrix[0].empty())
         {
             return false;
         }
-        bool isFind=false;
-        int row=0;
-        int col=matrix[0].size()-1;
-        while(row<matrix.size()&&col>=0)
+        bool isFind = false;
+        int row = 0;
+        int col = matrix[0].size() - 1;
+        while (row < matrix.size() && col >= 0)
         {
-            if(matrix[row][col]==target)
+            if (matrix[row][col] == target)
             {
-                isFind=true;
+                isFind = true;
                 break;
             }
-            else if(matrix[row][col]>target)
+            else if (matrix[row][col] > target)
             {
                 col--;
             }
@@ -27,6 +29,5 @@ public:
             }
         }
         return isFind;
-        
     }
 };
